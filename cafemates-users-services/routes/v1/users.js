90ed8a7f91d0ce9c4969047095de04922b06ab13
rehanchrl <rehanchoirul@gmail.com>
2018-05-24 13:44:28 +0700

    fix retrieve all users

--- cafemates-users-services/routes/v1/users.js
+++ cafemates-users-services/routes/v1/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const async = require('asyncawait/async')
 const await = require('asyncawait/await')
-const { retrieveUsersByEmail, upgradeLicence, updateUser, updateCover, retrieveUsersByID, registerUsers, deActiveUsers, deleteUsers, updatePassword, updateAvatar} = require('../../actions')
+const { retrieveUsersByEmail, upgradeLicence, updateUser, updateCover, retrieveUsers, retrieveUsersByID, registerUsers, deActiveUsers, deleteUsers, updatePassword, updateAvatar} = require('../../actions')
 const authentication = require('../../authentication')
 
 router.get('/', authentication, async(function(req, res, next) {
